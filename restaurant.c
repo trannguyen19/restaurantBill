ec3bb71e68713b52f5b9e422193fba0193e68b26
Tran Nguyen <tranhnguyen@cpp.edu>
2022-03-02 15:25:09 -0800

    Add source code

---
+++ restaurant.c
@@ -0,0 +1,24 @@
+/*
+Include Headers
+
+Declare main that will have will accept command line arguments
+    Declare  and initialize an Array of food name
+    Declare  and initialize an Array of prices
+    Declare  time_t
+    Declare tax, tip, total
+
+
+    Init random number generator
+
+    Validate the command line input (invalid numbers of argument and negative)
+
+        Randomly choose a meal
+
+        Compute tax, tip, and total
+
+        Display the meal cost
+        Display the tax amount
+        Display the tip amount 
+        Display the total cost
+
+*/
